480cabe6a7f032ef2981c659ce4962fc0deb104d
Shawn Hassen <Shassen89@gmail.com>
2018-07-05 16:05:26 -0400

    Javascript WIP

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 
-
-console.log('test');
-
-
-// game logic:
-
-const rootDiv = document.querySelector('#root');
-
 const arrowArr = [ { whiteUp: '../images/uparrow.png', redUp: '../images/reduparrow.png'},
                    { whiteRight: '../images/rightarrow.png', redRight: '../images/redrightarrow.png' },
                    { whiteDown: '../images/downarrow.png', redDown: '../images/reddownarrow.png' },
                    { whiteLeft: '../images/leftarrow.png', redLeft: '../images/redleftarrow.png' } ];
 
 //stackoverflow help here:
-document.onkeydown = function(e) {
+const keys = document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37:
-            console.log('left');
+            console.log(arrowArr[3], 'left');
             break;
         case 38:
-            console.log('up');
+            console.log(arrowArr[0], 'up');
             break;
         case 39:
-            console.log('right');
+            console.log(arrowArr[1], 'right');
             break;
         case 40:
-            console.log('down');
+            console.log(arrowArr[2], 'down');
             break;
     }
 };
+
+//key object:
+const key = [ { up: '38', right: '39', down: '40', left: '37' } ];
 
 // add event listeners to img's
 const arrowCount = document.querySelectorAll('.arrows');
@@ -38,41 +33,73 @@ for (let i = 0; i < arrowCount.length; i += 1) {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const rootDiv = document.querySelector('#root');
 
 // hidden window functions MAJOR help from w3schools.com:
 
 const hidContainer = document.querySelector('.content-container');
 const instructWindow = document.querySelector('#instructions');
 const leftArrow = document.querySelector('#left-arrow');
+const rightArrow = document.querySelector('#right-arrow');
+const downArrow = document.querySelector('#down-arrow');
+const startWindow = document.querySelector('#start-game');
+const footer = document.querySelector('footer');
+
+// left arrow functions and styling
 leftArrow.onclick = function() {
     instructWindow.style.display = "block";
     rootDiv.style.opacity = "0";
 
 }
-
 const span = document.querySelector('.close');
 span.onclick = function() {
     instructWindow.style.display = "none";
     rootDiv.style.opacity = "1";
 }
-
-window.onclick = function(e) {
+document.onclick = function(e) {
     if (e.target == instructWindow) {
         instructWindow.style.display = "none";
         rootDiv.style.opacity = "1";
     }
+}
+
+// right arrow functions and styling
+const startBody = document.querySelector('#start-body');
+rightArrow.onclick = function(e) {
+    //window.xxxx to handle default behavior of directional keys
+    e.preventDefault();
+    startWindow.style.display = "block";
+    startBody.style.padding = "100px";
+    rootDiv.style.opacity = "0";
+    footer.style.opacity = "0";
+}
+const timer = document.querySelector('#timer');
+const startSpan = document.querySelector('#start-close');
+startSpan.onclick = function() {
+    startWindow.style.display = "none";
+    rootDiv.style.opacity = "1";
+    footer.style.opacity = "1";
+    // timer.innerText = "Time: " + Date.now();
+}
+
+//game logic
+
+
+const gameArrows = document.querySelectorAll('.game-arrows');
+let playing = false;
+let currentSeq = 0;
+let startTime = 30;
+let seq = [];
+
+function startGame() {
+    playing = true;
+
+}
+
+function handleKeyPress() {
+    if (!playing) return;
+}
+
+function nextInSeq() {
+
 }
